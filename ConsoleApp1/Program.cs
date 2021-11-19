cff9b6b3411bf9f5f55441456c2ef804ed8b6e22
bell <we5m.szk@gmail.com>
2021-11-19 14:36:34 +0900

    11th.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("10th.");
+            Console.WriteLine("11th.");
         }
     }
 }
